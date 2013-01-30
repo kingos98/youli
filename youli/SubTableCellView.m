bf357ef0114c4104467c8dc215509834eed66653
sjun <songjun173@sohu.com>
2013-01-30 14:59:23 +0800

    加下拉选择组件

--- youli/SubTableCellView.m
+++ youli/SubTableCellView.m
@@ -17,11 +17,8 @@
         UIImageView *mainBgView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,320,200)];
         [mainBgView setImage:[UIImage imageNamed:@"sub_cell_bg.png"]];
         [self addSubview:mainBgView];
-//        self.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg.jpg"]];
-//        CGRect viewFrame = self.frame;
-//        viewFrame.size.height = 60 + 19;
-//        viewFrame.size.width = 320;
-//        self.frame = viewFrame;
+        
+        
     }
     return self;
 }
